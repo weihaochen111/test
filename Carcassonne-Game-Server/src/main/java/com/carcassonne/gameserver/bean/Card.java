3ea8cb91b8b02ff8b3081a59affe121a552571a6
Pilipala-cmy <70570316+Pilipala-cmy@users.noreply.github.com>
2021-04-30 00:06:09 +0800

    楼上是个帅哥

--- Carcassonne-Game-Server/src/main/java/com/carcassonne/gameserver/bean/Card.java
+++ Carcassonne-Game-Server/src/main/java/com/carcassonne/gameserver/bean/Card.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Bean;
  */
 public class Card {
     private Integer id;  //卡片的id，对应数据库的id
-    private String pictureURL;//这张卡片图片资源的URL
-    private String top_edge_id;
-    private String bot_edge_id;
-    private String lef_edge_id;
-    private String rig_edge_id;
+    private String pictureUrl;//这张卡片图片资源的URL
+    private String topEdgeId;
+    private String botEdgeId;
+    private String lefEdgeId;
+    private String rigEdgeId;
 
 
 
@@ -33,45 +33,62 @@ public class Card {
                 ", lef=" + lef +
                 ", rig=" + rig +
                 ", rotate='" + rotate + '\'' +
-                ", pictureURL='" + pictureURL + '\'' +
+                ", pictureURL='" + pictureUrl + '\'' +
                 '}';
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getTopEdgeId() {
+        return topEdgeId;
+    }
+
+    public void setTopEdgeId(String topEdgeId) {
+        this.topEdgeId = topEdgeId;
+    }
+
+    public String getBotEdgeId() {
+        return botEdgeId;
+    }
+
+    public void setBotEdgeId(String botEdgeId) {
+        this.botEdgeId = botEdgeId;
+    }
+
+    public String getLefEdgeId() {
+        return lefEdgeId;
+    }
+
+    public void setLefEdgeId(String lefEdgeId) {
+        this.lefEdgeId = lefEdgeId;
+    }
+
+    public String getRigEdgeId() {
+        return rigEdgeId;
+    }
+
+    public void setRigEdgeId(String rigEdgeId) {
+        this.rigEdgeId = rigEdgeId;
+    }
+
+    public String getRotate() {
+        return rotate;
+    }
+
+    public void setRotate(String rotate) {
+        this.rotate = rotate;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-    public String getTop_edge_id() {
-        return top_edge_id;
-    }
-
-    public void setTop_edge_id(String top_edge_id) {
-        this.top_edge_id = top_edge_id;
-    }
-
-    public String getBot_edge_id() {
-        return bot_edge_id;
-    }
-
-    public void setBot_edge_id(String bot_edge_id) {
-        this.bot_edge_id = bot_edge_id;
-    }
-
-    public String getLef_edge_id() {
-        return lef_edge_id;
-    }
-
-    public void setLef_edge_id(String lef_edge_id) {
-        this.lef_edge_id = lef_edge_id;
-    }
-
-    public String getRig_edge_id() {
-        return rig_edge_id;
-    }
-
-    public void setRig_edge_id(String rig_edge_id) {
-        this.rig_edge_id = rig_edge_id;
-    }
 
     public int getId() {
         return id;
@@ -129,19 +146,5 @@ public class Card {
         this.rig.setCityorroad(i);
     }
 
-    public String getRotate() {
-        return rotate;
-    }
 
-    public void setRotate(String rotate) {
-        this.rotate = rotate;
-    }
-
-    public String getPictureURL() {
-        return pictureURL;
-    }
-
-    public void setPictureURL(String pictureURL) {
-        this.pictureURL = pictureURL;
-    }
 }
